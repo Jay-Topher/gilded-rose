6a0bd2f2265ba14a17129b16e88f12fb7b4dcd32
Jones Ogolo <jay.topher11@gmail.com>
2022-08-04 18:56:31 +0100

    added comments

--- index.ts
+++ index.ts
@@ -14,19 +14,23 @@ if (!updateCount && !requestCount) {
 const shop = new GildedRose(items);
 
 async function performCallsAndUpdate() {
+  // initially empty log.txt
   logToFile("");
   let logText = getLogText();
 
+  // while log.txt is empty or has some positive responses
   while (logText !== "0") {
     const newRequestTimes = Number(logText) === 0 ? requestCount : logText;
     const data = await repeatRequest(Number(newRequestTimes));
     logToFile(data.toString());
     logText = data.toString();
   }
+  // update quality after all responses are negative
   shop.updateQuality();
 }
 
 const updateCountNum = Number(updateCount);
+// perform calls for the amount specified
 for (let i = 0; i < updateCountNum; i++) {
   performCallsAndUpdate();
 }
